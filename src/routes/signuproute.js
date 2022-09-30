ddfb3a905a8a708b63cb3e174bfc051a9cab16e3
mashithaptp <107739698+mashithaptp@users.noreply.github.com>
2022-09-30 22:43:58 +0530

    final
    final

---
+++ src/routes/signuproute.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const signupRouter = express.Router();
+const user = require('../data/user');
+
+signupRouter.get('/',function(req,res){
+
+    res.render('signup',{});
+    
+})
+
+signupRouter.get("/adduser",function(req,res){
+    
+    var newuser = {
+        "uid":req.body("uid"),
+        "pwd":req.body("pwd")
+    };
+    console.log(newuser);
+    user.push(newuser);
+    console.log(user);
+    res.redirect("/login");
+})
+
+module.exports = signupRouter;
